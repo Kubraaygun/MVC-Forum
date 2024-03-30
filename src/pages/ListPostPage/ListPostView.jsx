fbd4017bfd4961a68f6d4888a38462d856f16d55
kubraaygun <kubraaygun@outlook.com.tr>
2024-03-30 19:43:15 +0300

    scss ayarlari duzenlendi

--- src/pages/ListPostPage/ListPostView.jsx
+++ src/pages/ListPostPage/ListPostView.jsx
@@ -1,5 +1,15 @@
 const ListPostView = () => {
-  return <div>sdadasda</div>;
+  return (
+    <div className="container">
+      <div className="post">
+        <div className="info">
+          <h3>React MVC Pattern (Mimari)</h3>
+          <p>Mehmet</p>
+        </div>
+        <p className="text">MVC Mimari ile nasıl bileşenleri yönetirim?</p>
+      </div>
+    </div>
+  )
 };
 
 export default ListPostView;
